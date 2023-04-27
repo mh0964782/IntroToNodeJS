929efd6d0392dfd3f381c61bcdc660075fa960ca
HENRY <mh0964782@dt038573.otc.edu>
2023-04-27 10:06:53 -0500

    Added main.js and .env

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 import chalk from 'chalk';
+import main from './main.js';
+import {configs} from './main.js';
+import axios from 'axios';
+import {config} from 'dotenv';
+config();
+console.log(process.env.API_KEY);
 
 console.log(chalk.bgBlue("Hello,, World!"));
+main();
+configs();
+
+let api_url = `https://api.themoviedb.org/3/movie/550?api_key=${process.env.API_KEY}`;
+axios.get(api_url)
+.then(response => {
+    // console.log(response.data);
+});
---
+++ main.js
@@ -0,0 +1,7 @@
+export default () => {
+    console.log("defaut function.");
+}
+
+export const configs = () => {
+    console.log("I am the configs function.");
+}
